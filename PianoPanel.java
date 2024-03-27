873771ad5bdc82912be19751cb83b9ac59fc3908
bukson44 <165148383+bukson44@users.noreply.github.com>
2024-03-27 10:40:11 +0100

    Add files via upload

---
+++ PianoPanel.java
@@ -0,0 +1,72 @@
+package projekt;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class PianoPanel extends JPanel{
+	
+	JButton[] tone;
+	String[] tone_name;
+	JPanel[] panel;
+	int button_x,button_y;
+	String tone_type;
+	int panel_width=300;
+	
+	PianoPanel(String tone_type){
+		if(tone_type=="tones")
+		{
+			
+			button_x = 50;
+			button_y = 170;
+			String[] tone_name= {"C","D","E","F","G","A","H"};
+			tone = new JButton[tone_name.length];
+			panel = new JPanel[tone_name.length];
+			this.setSize(panel_width,button_y);
+			this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+			for(int i=0;i<tone_name.length;i++)
+			{
+				if(i==0) this.add(Box.createRigidArea(new Dimension(125,button_y)));
+				//if(i==2) this.add(Box.createRigidArea(new Dimension(70,button_y))); 
+				tone[i]=new JButton(tone_name[i]);
+				tone[i].setPreferredSize(new Dimension(button_x,button_y));
+				tone[i].setForeground(Color.black);
+				tone[i].setBackground(Color.white);
+				panel[i]=new JPanel();
+				panel[i].setSize(button_x,button_y);
+				panel[i].add(tone[i]);
+				this.add(panel[i]);
+				if(i==6) this.add(Box.createRigidArea(new Dimension(130,button_y))); 
+			}		
+		}
+		else
+		{	
+			
+			button_x = 50;
+			button_y = 170;
+			String[] tone_name= {"C#","D#","F#","G#","B"};
+			tone = new JButton[tone_name.length];
+			panel = new JPanel[tone_name.length];
+			this.setSize(panel_width,button_y);
+			this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+			for(int i=0;i<tone_name.length;i++)
+			{
+				if(i==0) this.add(Box.createRigidArea(new Dimension(150,button_y)));
+				if(i==2) this.add(Box.createRigidArea(new Dimension(60,button_y))); 
+				tone[i]=new JButton(tone_name[i]);
+				tone[i].setPreferredSize(new Dimension(button_x,button_y));
+				tone[i].setForeground(Color.white);
+				tone[i].setBackground(Color.black);
+				panel[i]=new JPanel();
+				panel[i].setSize(button_x,button_y);
+				panel[i].add(tone[i]);
+				this.add(panel[i]);
+				if(i==4) this.add(Box.createRigidArea(new Dimension(160,button_y))); 
+			}		
+		}	
+	}
+}
---
+++ PianoSimulator.java
@@ -0,0 +1,244 @@
+package projekt;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JSlider;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class PianoSimulator extends JFrame implements ActionListener {
+    private final int SIZE_X = 1000;
+    private final int SIZE_Y = 800;
+    static final int SLIDER_MIN = 0;
+    static final int SLIDER_MAX = 100;
+    static final int SLIDER_INIT = 0;
+
+    
+    public static void main(String[] args) {
+        PianoSimulator pianoSimulator = new PianoSimulator();
+        pianoSimulator.setVisible(true);
+
+    }
+    
+    public PianoSimulator() throws HeadlessException {
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.setSize(SIZE_X,SIZE_Y);
+        createRightPanel(BorderLayout.EAST);
+        createLeftPanel(BorderLayout.CENTER);
+        createMenu();
+
+        GenerationMethod generationMethod = GenerationMethod.SIMPLE;
+
+    }
+
+    
+   
+	void createRightPanel(String layout) {
+		
+		//CAŁY BOCZNY PANEL (PRAWY)
+		JPanel rightPanel = new JPanel(new GridLayout(3, 1));
+		rightPanel.setSize(500,400);
+		
+	    //PANEL POJEYNCZYCH DZWIEKOW I SLIDERA
+	    JPanel simplePanel = new JPanel(new GridLayout(2,1));
+	    
+	    simplePanel.setSize(400, 400);
+	
+	    //slider
+	    JSlider slider = new JSlider(JSlider.HORIZONTAL, SLIDER_MIN, SLIDER_MAX, SLIDER_INIT);
+	    slider.setSize(400, 200);
+	    slider.setMajorTickSpacing(20);
+	    slider.setMinorTickSpacing(10);
+	    slider.setPaintTicks(true);
+	    slider.setPaintLabels(true);
+	    //slider.addChangeListener(new SliderChangeListener(slider));
+	    simplePanel.add(slider);
+	    
+
+	    JRadioButton simpleButton = new JRadioButton();
+	    simpleButton.setLabel("POJEDYNCZE DZWIEKI         ");
+	    simpleButton.setFont(new Font("Courier", Font.BOLD,20));
+	    simplePanel.add(simpleButton);
+	    simpleButton.setSelected(true);
+	    rightPanel.add(simplePanel);
+	    
+	    //PANEL INTERWALOW
+	    JPanel intervalPanel = new JPanel(new GridLayout(4,1));
+	    
+	    
+	    JRadioButton intervalButton = new JRadioButton();
+	    intervalButton.setLabel("INTERWALY");
+	    intervalButton.setFont(new Font("Courier", Font.BOLD,20));
+	    intervalPanel.add(intervalButton);
+	    
+	    JLabel startingNoteLabel = new JLabel("Dzwiek poczatkowy ");
+	    //startingNoteLabel.setFont(new Font("Courier", Font.BOLD,10));
+	    intervalPanel.add(startingNoteLabel);
+	    
+	    //JComboBox box = new JComboBox();
+	    
+	    
+	    JLabel intervalLabel = new JLabel("Interwal ");
+	    intervalPanel.add(intervalLabel);
+	    
+	    JLabel emptyLabel = new JLabel("");
+	    intervalPanel.add(emptyLabel);
+	    
+	    
+	    rightPanel.add(intervalPanel);
+	    
+	    //PANEL AKORDOW
+	    JPanel chordPanel = new JPanel(new GridLayout(6,1));
+	    
+	    JRadioButton chordButton = new JRadioButton();
+	    chordButton.setLabel("AKORDY");
+	    chordButton.setFont(new Font("Courier", Font.BOLD,20));
+	    chordPanel.add(chordButton);
+	    
+	    JLabel scaleLabel = new JLabel("Skala ");
+	    chordPanel.add(scaleLabel);
+	    
+	    JLabel keyLabel = new JLabel("Tonacja ");
+	    chordPanel.add(keyLabel);
+	    
+	    JLabel triadLabel = new JLabel("Funkcja (triada harmoniczna) ");
+	    chordPanel.add(triadLabel);
+	    
+	    
+	    //PANEL TRIADY
+	    JPanel triadPanel = new JPanel(new GridLayout(3,1));
+	    
+	    JRadioButton TButton = new JRadioButton();
+	    TButton.setLabel("TONIKA");
+	    triadPanel.add(TButton);
+	    
+	    JRadioButton SButton = new JRadioButton();
+	    SButton.setLabel("SUBDOMINANTA");
+	    triadPanel.add(SButton);
+	    
+	    JRadioButton DButton = new JRadioButton();
+	    DButton.setLabel("DOMINANTA");
+	    triadPanel.add(DButton);
+	    
+	    chordPanel.add(triadPanel);
+	    
+	    rightPanel.add(chordPanel);
+	    
+	  //sic = simple, interval, chord
+	    ButtonGroup sicGroup = new ButtonGroup();
+	    sicGroup.add(simpleButton);
+	    sicGroup.add(intervalButton);
+	    sicGroup.add(chordButton);
+	     
+	    
+	    //TSD = tonika, subdominanta, dominanta
+	    ButtonGroup TSDGroup = new ButtonGroup();
+	    TSDGroup.add(TButton);
+	    TSDGroup.add(SButton);
+	    TSDGroup.add(DButton);
+	
+	    RadioListener radioListener = new RadioListener();
+	    simpleButton.addActionListener(radioListener);
+	    intervalButton.addActionListener(radioListener);
+	    chordButton.addActionListener(radioListener);
+	    
+	    this.add(rightPanel, layout);
+	}
+	
+	
+	void createLeftPanel(String layout) {
+    	
+    	//WSZYSTKO POZOSTAŁE
+    	JPanel leftPanel = new JPanel(new GridLayout(2, 1));
+    	leftPanel.setSize(900,800);
+    	PlotPanel plot = new PlotPanel();
+    	plot.setBackground(Color.lightGray);
+    	leftPanel.add(plot);
+    	JPanel keyboardPanel = new JPanel(new GridLayout(2,1));
+    	PianoPanel semitonesKeyboard = new PianoPanel("semitones");
+    	PianoPanel tonesKeyboard = new PianoPanel("tones");
+    	keyboardPanel.add(semitonesKeyboard);
+    	keyboardPanel.add(tonesKeyboard);
+    	leftPanel.add(keyboardPanel);
+    	this.add(leftPanel, layout);
+    	//JLabel chartLabel = new JLabel("Dzwiek poczatkowy ");
+        //leftPanel.add(chartLabel);
+    	
+    }
+	
+	void createMenu() {
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem;
+
+        JRadioButtonMenuItem rbMenuItem;
+        JCheckBoxMenuItem cbMenuItem;
+
+//Create the menu bar.
+        menuBar = new JMenuBar();
+
+//Build the first menu.
+        menu = new JMenu("Menu");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+
+
+        menuItem = new JMenuItem("Wybierz język");
+        menuItem.setMnemonic(KeyEvent.VK_B);
+        menu.add(menuItem);
+        
+        menuItem = new JMenuItem("Kolor tła");
+        menuItem.setMnemonic(KeyEvent.VK_B);
+        menu.add(menuItem);
+        
+
+        
+        menu.add(menuItem);
+
+        this.setJMenuBar(menuBar);
+    }
+
+
+public class RadioListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+//        switch(arg0.getActionCommand()) {
+//            case "Regular": generationMethod = GenerationMethod.REGULAR; break;
+//            case "Random": generationMethod = GenerationMethod.RANDOM; break;
+//        }
+        revalidate();
+        repaint();
+    }
+
+}
+
+enum GenerationMethod {
+    SIMPLE, INTERVAL, CHORD
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+}
