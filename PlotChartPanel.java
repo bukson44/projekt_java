cd1e4ac90a56f12e6bc4372eba0db67be92fa373
bukson44 <165148383+bukson44@users.noreply.github.com>
2024-05-05 14:16:36 +0200

    Update and rename PlotPanel.java to PlotChartPanel.java

---
+++ PlotChartPanel.java
@@ -0,0 +1,55 @@
+package projekt;
+
+import java.util.List;
+
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+/**
+ * @author Jakub Bukowski
+ */
+public class PlotChartPanel extends JPanel {
+	String plotTitle = "fala akustyczna";
+	int option=1;
+	List<XYSeries> dataSet;
+     PlotChartPanel(JPanel aimPanel){
+         XYSeries dataSet2= new XYSeries("seria 2");
+
+             for (double i=0; i <100; i+=0.05) dataSet2.add(i,Math.sin(i));
+
+             XYSeriesCollection xySeriesCollection = new XYSeriesCollection(dataSet2); 
+           
+             XYDataset dataset = xySeriesCollection;      
+
+             JFreeChart lineGraph = ChartFactory.createXYLineChart 
+                         ("",  // Title 
+                           "Czas [s]",           // X-Axis label 
+                           "Wychylenie z położenia równowagi",           // Y-Axis label 
+                           dataset,          // Dataset 
+                           PlotOrientation.VERTICAL,        //Plot orientation 
+                           true,                //show legend 
+                           true,                // Show tooltips 
+                           false               //url show 
+                          ); 
+
+             ChartPanel chartPanel = new ChartPanel(lineGraph);
+             aimPanel.add(chartPanel);
+     }
+     
+     void setData(){
+    	 if(option==1){
+    		 
+    	 }
+    	 else if(option==2){
+    		 
+    	 }
+    	 else if(option==3){
+    		 
+    	 }
+     }
+}
